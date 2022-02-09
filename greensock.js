110e9a8ec8b73405349a238048d636a36510832b
addterra <98066696+addterra@users.noreply.github.com>
2022-02-09 12:08:06 -0700

    Update greensock.js

--- greensock.js
+++ greensock.js
@@ -1,66 +1,30 @@
 
-
-
-
 gsap.registerPlugin(ScrollTrigger); 
 
 
-//attempting a scroll trigger & it worked!!!!  I am a genius!  
-////
 
+/// making a gsap timeline 
 
-// circle 1 fade in 
-gsap.to("#circle", {duration: 4, ease: "circ.out", width:650, x:325, y:-325, opacity: 1, 
-        scrollTrigger: {  
-            trigger: ".container2",
-            start: 'top top-=200',
-            end: 'bottom',
-            scrub: true 
-        }
-});
+//// insert at start of previous animation: "<"
 
-//object 1 fade in 
-gsap.to("#object1", {duration: 4, ease: "circ.out", width:250, x:125, y:-125, opacity: 1,
+var tl = gsap.timeline( {
     scrollTrigger: {  
         trigger: ".container2",
-        start: 'top top-=200',
-        end: 'bottom',
+        start: 'top top-=100',
+        end: 'bottom', 
         scrub: true 
     }
 });
-
-////second wave of animation 
-
-///circle 1 fade out 
-gsap.to("#circle", {duration: 5, ease: "power1.out", width: 2000, x:1000, y:-1000,
-    scrollTrigger: {  
-        trigger: ".container3",
-        start: 'top top-=30px',
-        scrub: true 
-    }
-});
-
-//object 1 fade out 
-gsap.to("#object1", {duration: 5, ease: "power1.out", width:1950, x: 2700, y:-825,
-    scrollTrigger: {  
-        trigger: ".container3",
-        start: 'top top-=30px',
-        scrub: true 
-    }
-});
-
-//circle 2 fade in 
-gsap.to("#circle2", {duration: 6, ease: "circ.out", width:650, x: 325, y:-325, opacity: 1, 
-    scrollTrigger: {  
-        trigger: ".container3",
-        start: 'top top-=30px',
-        scrub: true 
-    }
-});
-
-
-
-
+////circle1 fade in 
+tl.to("#circle", {duration: 4, ease: "circ.out", width:650, x:325, y:-325, opacity: 1})
+//// object1 fade in 
+tl.to("#object1", {duration: 4, ease: "circ.out", width:250, x:125, y:-125, opacity: 1}, "<") 
+/// circle1 fade out 
+tl.to("#circle", {duration: 5, ease: "power1.out", width: 2000, x:1000, y:-1000})
+///object1 fade out 
+tl.to("#object1", {duration: 5, ease: "power1.out", width:1950, x: 2700, y:-825}, "<")
+//circle2 fadein 
+tl.to("#circle2", {duration: 6, ease: "circ.out", width:650, x: 325, y:-325, opacity: 1}, "<");
 
 
 
