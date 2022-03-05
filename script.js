f27695e424beba65d72fb98ab3d83284d19eada7
addterra <98066696+addterra@users.noreply.github.com>
2022-03-04 22:47:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+// activate scrolltrigger 
+gsap.registerPlugin(ScrollTrigger); 
+
+
+// get screen width 
+
+var width = screen.width;
+var half = (width / 2)* -1; 
+
+var height = (screen.height) * -1;
+
+
+//gsap animating:
+
+var tl = gsap.timeline( {
+    scrollTrigger: {  
+        trigger: ".please",
+        start: 'top top-=100',
+        end: 'bottom', 
+        scrub: true 
+    }
+});
+
+tl.to(".airplane", {duration: 1, ease: 'power 1', x:half});
+tl.to(".ball_line", {duration: 1, ease: 'power 1', x:half}, "<");
+tl.to(".section1", {duration: 1, ease: 'power 1', y:height});
+
+function slide() {
+
+gsap.to(".airplane", {duration: 1, ease: 'power 1', x:"-638px"});
+gsap.to(".ball_line", {duration: 1, ease: 'power 1', x:"-638px"});
+
+};
+
+function slide2() {
+    
+};
