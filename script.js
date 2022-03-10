f972dbeda19f7ff9663693681895598bfbb664db
addterra <98066696+addterra@users.noreply.github.com>
2022-03-09 18:44:02 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,26 @@
 gsap.registerPlugin(ScrollTrigger); 
 
 
-// get screen width 
+// screen width and height variables for responsive animations  
 
-var width = screen.width;
+// screen width 
+var width = window.innerWidth;
+// negative 1/2 screen width 
 var half = (width / 2)* -1; 
+// positive 1/2 screen width 
+var half_pos = width/2;
+// negative half screen width minus caption width 
+var left_caption = ((width/2) - (width * 0.203)) * -1;
+// window height negative
+var height = (window.innerHeight) * -1;
+// window height double 
+var height2 = height * 2; 
 
-var height = (screen.height) * -1;
+
+// reload browser on window resize (in order to recalculate screen width 
+// and height for accurate positioning) 
+
+window.onresize = function(){ location.reload(); }
 
 
 //gsap animating:
@@ -22,6 +36,37 @@ var tl = gsap.timeline( {
     }
 });
 
-tl.to(".airplane", {duration: 1, ease: 'power 1', x:half});
-tl.to(".ball_line", {duration: 1, ease: 'power 1', x:half}, "<");
-tl.to(".section1", {duration: 1, ease: 'power 1', y:height});
+
+// entire opening section scrolls up 
+tl.to(".opening_section", {duration: 1, ease: 'power 1', y:height})
+
+// entire first section scrolls into view 
+tl.to(".section1", {duration: 1, ease: 'power 1', y:height}, "<")
+
+// first image & caption move from 50% of screen width to full screen, moving left 
+tl.to(".image1", {duration: 0.5, ease: 'power 1', x:half})
+tl.to(".caption_box", {duration: 0.5, ease: 'power 1', x:half}, "<")
+
+// entire first section scrolls up 
+tl.to(".section1", {duration: 1, ease: 'power 1', y:height2})
+
+// entire second section scrolls into view 
+tl.to(".section2", {duration: 1, ease: 'power 1', y:height}, "<")
+
+// second image & caption (& clip path) move from 50% of screen width to full screen, moving right 
+tl.to(".image2", {duration: 0.5, ease: 'power 1', x:half_pos})
+tl.to(".caption_box2", {duration: 0.5, ease: 'power 1', x:left_caption}, "<")
+tl.to(".clip_box", {duration: 0.5, ease: 'power 1', clipPath: 'inset(0% 0% 0% 0%)'}, "<")
+
+// entire second section scrolls up 
+tl.to(".section2", {duration: 1, ease: 'power 1', y:height2})
+
+//entire third section scrolls into view 
+tl.to(".section3", {duration: 1, ease: 'power 1', y:height}, "<");
+
+// third image & caption move from 50% of screen width to full screen, moving left 
+tl.to(".image3", {duration: 0.5, ease: 'power 1', x:half})
+tl.to(".caption_box3", {duration: 0.5, ease: 'power 1', x:half}, "<")
+
+// entire third section scrolls up 
+tl.to(".section3", {duration: 1, ease: 'power 1', y:height2}); 
