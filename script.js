2bc3a0a28604e191c846b8ab7a949b15383dd2e5
addterra <98066696+addterra@users.noreply.github.com>
2022-03-17 23:39:33 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ window.onresize = function(){ location.reload(); }
 
 //gsap animating:
 
+//major timeline
 var tl = gsap.timeline( {
     scrollTrigger: {  
         trigger: ".please",
@@ -34,6 +35,16 @@ var tl = gsap.timeline( {
         end: 'bottom', 
         scrub: true 
     }
+});
+
+//static section timeline 
+var tl2 = gsap.timeline( {
+  scrollTrigger: {  
+      trigger: ".section3",
+      start: 'top',
+      end: 'bottom', 
+      scrub: true 
+  }
 });
 
 
@@ -44,53 +55,53 @@ tl.to(".opening_section", {duration: 1, ease: 'linear', y:height})
 // logo goes from full size into the corner
 tl.to(".logo", {duration: 1, ease: 'power 1', top: -10, left: 0, width: "7vw", padding: 25}, "<")
 //subtitles disappear 
-tl.to(".subtitle", {duration: 0.1, ease: 'power 1', opacity: 0}, "<")
-tl.to(".caption_boxO", {duration: 0.1, ease: 'power 1', opacity: 0}, "<")
+tl.to(".subtitle", {duration: 0.2, ease: 'power 1', opacity: 0}, "<")
+tl.to(".caption_boxO", {duration: 0.2, ease: 'power 1', opacity: 0}, "<")
 // entire first section scrolls into view 
 tl.to(".section1", {duration: 1, ease: 'linear', y:height}, "<")
 
 // first image & caption move from 50% of screen width to full screen, moving left 
-tl.to(".image1", {duration: 0.5, ease: 'power 1', x:half})
-tl.to(".caption_box", {duration: 0.5, ease: 'power 1', x:half}, "<")
+tl.to(".image1", {duration: 1, ease: 'power 1', x:half, delay: 0.5})
+tl.to(".caption_box", {duration: 1, ease: 'power 1', x:half}, "<")
 
 // entire first section scrolls up 
-tl.to(".section1", {duration: 1, ease: 'power 1', y:height2})
+tl.to(".section1", {duration: 2, ease: 'none', y:height2})
 
 // entire second section scrolls into view 
-tl.to(".section2", {duration: 1, ease: 'power 1', y:height}, "<")
+tl.to(".section2", {duration: 2, ease: 'none', y:height}, "<")
 
 // second image & caption (& clip path) move from 50% of screen width to full screen, moving right 
-tl.to(".image2", {duration: 0.5, ease: 'power 1', x:half_pos})
-tl.to(".caption_box2", {duration: 0.5, ease: 'power 1', x:left_caption}, "<")
-tl.to(".clip_box", {duration: 0.5, ease: 'power 1', clipPath: 'inset(0% 0% 0% 0%)'}, "<")
+tl.to(".image2", {duration: 1, ease: 'power 1', x:half_pos,delay: 0.5 })
+tl.to(".caption_box2", {duration: 1, ease: 'power 1', x:left_caption}, "<")
+tl.to(".clip_box", {duration: 1, ease: 'power 1', clipPath: 'inset(0% 0% 0% 0%)'}, "<")
 
 // entire second section scrolls up 
-tl.to(".section2", {duration: 1, ease: 'power 1', y:height2})
+tl.to(".section2", {duration: 2, ease: 'power 1', y:height2})
 
 //entire third section scrolls into view 
-tl.to(".section3", {duration: 1, ease: 'power 1', y:height}, "<");
+tl.to(".section3", {duration: 2, ease: 'power 1', y:height}, "<");
 
 // third image & caption move from 50% of screen width to full screen, moving left 
-tl.to(".image3", {duration: 0.5, ease: 'power 1', x:half})
-tl.to(".caption_box3", {duration: 0.5, ease: 'power 1', x:half}, "<")
+tl.to(".image3", {duration: 1, ease: 'power 1', x:half, delay: 0.5})
+tl.to(".caption_box3", {duration: 1, ease: 'power 1', x:half}, "<")
 
 // entire third section scrolls up 
-tl.to(".section3", {duration: 1, ease: 'power 1', y:height2}) 
+tl.to(".section3", {duration: 2, ease: 'none', y:height2}) 
 
 // static section scrolls up 
-tl.to(".static_section", {duration: 1, ease: 'power 1', y:height2}, "<");
+tl.to(".static_section", {duration: 2, ease: 'none', y:height}, "<");
 
 
 //// animated menu 
 
 function menuIn() {
-
     gsap.to("#circle_menu", {duration: 1.5, ease: 'back', x: -200, y: 200});
     gsap.to("#ham", {duration: 0.5, ease: 'power 1', opacity: 0});
   
   };
   
-  function menuOut() {
+  
+function menuOut() {
     gsap.to("#circle_menu", {duration: 2, ease: 'back', x: 200, y: -200});
     gsap.to("#ham", {duration: 0.5, ease: 'power 1', opacity: 1});
-  };
+};
