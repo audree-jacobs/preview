ff406ac94ef2f61695f83724b7b99c1110f96cfc
addterra <98066696+addterra@users.noreply.github.com>
2022-04-22 19:05:58 -0600

    Add files via upload

--- Landing Page Redesign/script.js
+++ Landing Page Redesign/script.js
@@ -1,12 +1,44 @@
-/// circle menu animation 
+// register scroll trigger
+gsap.registerPlugin(ScrollTrigger); 
 
+
+//variables 
+var vh = (screen.height)/100;
+var vw = (screen.width)/100;
+// size of circle menu 
+var tenvw = vw * 14; 
+var half = vw * 50; 
+var full = vw * 100; 
+var fullh = vh * 140; 
+
+
+/// circle menu animation 
 function menuIn() {
-    gsap.to("#circle_menu", {duration: 1.5, ease: 'back', x: -250, y: 250});
+    gsap.to("#circle_menu", {duration: 1.5, ease: 'back', x: -tenvw, y: tenvw});
     gsap.to("#menuIcon", {duration: 0.5, ease: 'power 1', opacity: 0});
   };
   
-  
 function menuOut() {
-    gsap.to("#circle_menu", {duration: 2, ease: 'back', x: 250, y: -250});
+    gsap.to("#circle_menu", {duration: 2, ease: 'back', x: tenvw, y: -tenvw});
     gsap.to("#menuIcon", {duration: 0.5, ease: 'power 1', opacity: 1});
 };
+
+
+// scroll trigger for text and image movement 
+
+var tl = gsap.timeline( {
+  scrollTrigger: {  
+      trigger: ".section2",
+      start: 'top top-=200',
+      end: 'bottom bottom', 
+      scrub: true 
+  }
+});
+
+tl.to(".movingText1", {duration: 0.5, ease: 'linear', x: -full}); 
+tl.to(".movingText2", {duration: 0.5, ease: 'linear', x: -half}, "<");
+tl.to(".movingText2", {duration: 0.5, ease: 'linear', x: -full}, 1);
+tl.to(".movingText3", {duration: 0.5, ease: 'linear', x: -half}, "<");
+tl.to(".movingText3", {duration: 0.5, ease: 'linear', x: -half});
+tl.to("#firstImage", {duration: 0.5, ease: 'linear', x: -half});
+tl.to(".subsection2", {duration: 0.5, ease: 'linear', y: -fullh});
